fe3a1e8634631b30f869ce8cb2539526c2e0090a
Neeraj Kashyap <nkashy1@gmail.com>
2021-12-17 08:30:40 -0800

    Added ERC20WithCommonStorage

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dao
 Moonstream DAO
+
+## Development environment
+
+### VSCode setup
+
+If you are using the Solidity extension in VSCode, merge the following snippet into your settings.json:
+
+```json
+{
+    "solidity.remappings": [
+        "@openzeppelin-contracts/=<path to your home directory>/.brownie/packages/OpenZeppelin/openzeppelin-contracts@4.3.2"
+    ]
+}
+```
--- contracts/erc20/LibERC20.sol
+++ contracts/erc20/LibERC20.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-library LibDiamond {
+library LibERC20 {
     bytes32 constant ERC20_STORAGE_POSITION =
         keccak256("moonstreamdao.eth.storage.erc20");
 
     struct ERC20Storage {
-        address controller;
-        uint256 totalSupply;
         string name;
         string symbol;
         mapping(address => uint256) balances;
         mapping(address => mapping(address => uint256)) allowances;
+        address controller;
+        uint256 totalSupply;
     }
 
     function erc20Storage() internal pure returns (ERC20Storage storage es) {
@@ -26,19 +26,19 @@ library LibDiamond {
         address indexed newController
     );
 
-    function setController(address _newController) internal {
+    function setController(address newController) internal {
         ERC20Storage storage es = erc20Storage();
         address previousController = es.controller;
-        es.controller = _newController;
-        emit ControlTransferred(previousController, _newController);
+        es.controller = newController;
+        emit ControlTransferred(previousController, newController);
     }
 
     function getController()
         internal
         view
-        returns (address contractController_)
+        returns (address contractController)
     {
-        contractController_ = erc20Storage().controller;
+        contractController = erc20Storage().controller;
     }
 
     function enforceIsController() internal view {
