4c4b0cf223e7d0f52529b439463d1f714d6bd891
Tim Pechersky <tim.pechersky@gmail.com>
2022-02-15 18:01:06 +0000

    tests wip

--- dao/test_PoolControllerEnumerateMigration.py
+++ dao/test_PoolControllerEnumerateMigration.py
@@ -5,12 +5,62 @@ from brownie import accounts
 from brownie.exceptions import VirtualMachineError
 from sqlalchemy import true
 
-from . import TerminusFacet, PoolControllerEnumeration
+from . import TerminusFacet, PoolControllerEnumeration, TerminusFacetFixture, TerminusInitializerFixture
 from .core import facet_cut
 from .test_core import MoonstreamDAOSingleContractTestCase
 
 
 class TestPoolControllerEnumerationMigration(MoonstreamDAOSingleContractTestCase):
+        @classmethod
+        def setUpClass(cls) -> None:
+            super().setUpClass()
+
+    def test_migration_from_old_state(self):
+
+        # old state
+        fixture_initializer = TerminusInitializerFixture.TerminusInitializerFixture(None)
+        fixture_initializer.deploy({"from": accounts[0]})
+
+        fixture_terminus_facet = TerminusFacetFixture.TerminusFacetFixture(None)
+        fixture_terminus_facet.deploy({"from": accounts[0]})
+
+        # deploy, intialize, setup and test new diamond contract
+
+        # facet_cut fixtures onto newly created diamond
+
+        # test that facets are fixtures
+
+        # run standard facet tests
+
+        # replace facet with PoolControllerEnumeration update
+
+        initializer = PoolControllerEnumeration.PoolControllerEnumeration(None)
+        initializer.deploy({"from": accounts[0]})
+
+        terminus_facet = TerminusFacet.TerminusFacet(None)
+        terminus_facet.deploy({"from": accounts[0]})
+
+        diamond_address = self.contracts["Diamond"]
+        facet_cut(
+            diamond_address,
+            "TerminusFacet",
+            terminus_facet.address,
+            "add",
+            {"from": accounts[0]},
+            initializer.address,
+        )
+
+        # test that pool enumeration migrated successfully
+        diamond_terminus = TerminusFacet.TerminusFacet(diamond_address)
+        for poolId in range(diamond_terminus.total_pools()):
+                controller = diamond_terminus.terminus_pool_controller(poolId)
+
+
+
+
+
+
+
     def test_storage_migration(self):
         initializer = PoolControllerEnumeration.PoolControllerEnumeration(None)
         initializer.deploy({"from": accounts[0]})
@@ -35,15 +85,7 @@ class TestPoolControllerEnumerationMigration(MoonstreamDAOSingleContractTestCase
         controllerPools1 = diamond_terminus.get_controller_pools(accounts[0].address)
         self.assertEqual(len(controllerPools0)+1, len(controllerPools1))
         self.assertDictContainsSubset(controllerPools0, controllerPools1)
-        for poolId in range(diamond_terminus.total_pools()):
-            controller = diamond_terminus.terminus_pool_controller(poolId)
-            is_enumerated = False
-            if poolId in diamond_terminus.controllerPools1:
-                is_enumerated = True
-            if controller == accounts[0].address:
-                self.assertTrue(is_enumerated)
-            else:
-                self.assertFalse(is_enumerated)
+
 
     def test_pool_enum_after_transfer(self):
         diamond_address = self.contracts["Diamond"]
